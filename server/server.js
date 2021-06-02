dfd8fd6a50f3723e4948f3448496af0933753b26
HipsterSantos <santoscampos269@gmail.com>
2021-06-02 14:07:53 +0100

    adding somet changes

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-import express from 'express'; 
+import express from 'express';
 import mongoose from 'mongoose';
 const mongodb = 'mongodb+srv://nagato:Pensador43@cluster1.qwmzi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const uri  = 'mongodb://localhost/semil';
@@ -22,6 +22,36 @@ const orderSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectID,
         ref:'User'}
 })
+
+const userModel = mongoose.model('User', useSchema);
+const orderModel  = mongoose.model('Order',orderSchema);
+
+
+async function inserOneOrder(user){
+
+const order = orderModel({
+  ...order
+})
+return awit order.save();
+}
+async function inserManyOder(col){
+  const order = orderModel({...col})
+  return awit order.save();
+}
+
+async function insertOneUsers(user){
+  const user = orderModel({
+    user
+  })
+
+  return await user.save();
+}
+async function insertManyUsers(users){
+  const users = orderModel({
+    ...users
+  })
+  return await user.save();
+}
 app.get('',async(req,res)=>{
     console.log('testing mongodb performance by doing aggression and other stuffs')
 })
